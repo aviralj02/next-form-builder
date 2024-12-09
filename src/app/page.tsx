d595e01da7f547c97af91f9bf8acf6184aca1a74
aviralj02 <aviraljain1311@gmail.com>
2024-12-09 16:18:16 +0530

    chore: update env + minor css changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,23 +17,21 @@ export default function Home() {
         </Link>
       </header>
 
-      <main className="flex-1 h-fit">
+      <main className="flex-1 w-full">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Create Forms with Ease
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Build simple, powerful forms in minutes. No authentication
-                  required.
-                </p>
-              </div>
-              <Link href="/builder">
-                <Button buttonType={ButtonType.ACTIVE}>Get Started</Button>
-              </Link>
+          <div className="flex flex-col items-center space-y-4 text-center w-full">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                Create Forms with Ease
+              </h1>
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                Build simple, powerful forms in minutes. No authentication
+                required.
+              </p>
             </div>
+            <Link href="/builder">
+              <Button buttonType={ButtonType.ACTIVE}>Get Started</Button>
+            </Link>
           </div>
         </section>
       </main>
